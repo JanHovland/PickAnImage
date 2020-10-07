34f04a0b0b122124e5c57afff94f1652acb7fbb3
Jan Hovland <jan.hovland@lyse.net>
2020-10-07 20:26:39 +0200

    Update

--- PickAnImage/ContentView.swift
+++ PickAnImage/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     
     @State var showImagePicker: Bool = false
     @State var image: UIImage?
-
+    
     var body: some View {
         VStack {
             if image != nil {
@@ -25,7 +25,6 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 80, height: 80, alignment: .center)
                     .clipShape(Circle())
-                    
             }
             Button(
                 action: { ImgPicker() },
@@ -59,53 +58,60 @@ struct ContentView: View {
 }
 
 public struct ImagePickerView: UIViewControllerRepresentable {
-
+    
     private let sourceType: UIImagePickerController.SourceType
     private let onImagePicked: (UIImage) -> Void
     @Environment(\.presentationMode) private var presentationMode
-
+    
     public init(sourceType: UIImagePickerController.SourceType, onImagePicked: @escaping (UIImage) -> Void) {
-        self.sourceType = sourceType
+        self.sourceType = .photoLibrary
         self.onImagePicked = onImagePicked
     }
-
+    
     public func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
-        picker.sourceType = self.sourceType
+        picker.sourceType = .photoLibrary
+        /// Muliggjør utvalg på bildet
+        picker.allowsEditing = true
         picker.delegate = context.coordinator
         return picker
     }
-
+    
     public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-
+    
     public func makeCoordinator() -> Coordinator {
         Coordinator(
             onDismiss: { self.presentationMode.wrappedValue.dismiss() },
             onImagePicked: self.onImagePicked
         )
     }
-
+    
     final public class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+        
         private let onDismiss: () -> Void
         private let onImagePicked: (UIImage) -> Void
-
+        
         init(onDismiss: @escaping () -> Void, onImagePicked: @escaping (UIImage) -> Void) {
             self.onDismiss = onDismiss
             self.onImagePicked = onImagePicked
         }
-
+        
         public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let image = info[.originalImage] as? UIImage {
+            /// Her hentes hele bildet:
+            //            if let image = info[.originalImage] as? UIImage {
+            //                self.onImagePicked(image)
+            //            }
+            /// Her hentes det editerte bildet_
+            if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
                 self.onImagePicked(image)
             }
             self.onDismiss()
         }
-
+        
         public func imagePickerControllerDidCancel(_: UIImagePickerController) {
             self.onDismiss()
         }
-
+        
     }
-
+    
 }
