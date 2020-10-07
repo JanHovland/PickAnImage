dadbc9b6e69f366b4bfcd9d6c8a3bd02f95368c3
Jan Hovland <jan.hovland@lyse.net>
2020-10-07 18:20:00 +0200

    Initial Commit

---
+++ PickAnImage/PickAnImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickAnImageApp.swift
+//  PickAnImage
+//
+//  Created by Jan Hovland on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct PickAnImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
